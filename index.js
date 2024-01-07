88737df4d3689ee697d2d5aad8a30c5cd5ec8380
waterfay <waterfay0.0@gmail.com>
2024-01-07 17:19:19 +0800

    123

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");//載入Express
 const app=express();//建立application物件
 //處理來自路徑/的連線
 app.get("/",function(req,res){
-    res.send("Hello Express.js");
+    res.send("<h3>Hello Express.js</h3><u>這就是HTML</u>");
 });
 //處理來自路徑/text的連線
 app.get("/test",function(req,res){
